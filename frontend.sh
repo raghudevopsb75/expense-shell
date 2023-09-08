8ab53b980303bd6f13c56d72c5f031c8f8800fe3
Raghu K <raghuk.vit@gmail.com>
2023-09-08 08:18:51 +0530

    Expense Scripts

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 source common.sh
+component=frontend
 
 echo Installing Nginx
 dnf install nginx -y >>$log_file
@@ -9,13 +10,9 @@ cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* >>$log_file
 
-echo Download Frontend Code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
-
 cd /usr/share/nginx/html
 
-echo Extracting Frontend Code
-unzip /tmp/frontend.zip >>$log_file
+download_and_extract
 
 echo Starting Nginx Service
 systemctl enable nginx >>$log_file
