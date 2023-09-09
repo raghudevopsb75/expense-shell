84ae5e5ae12e1a89411f49163178ab37f9750111
Raghu K <raghuk.vit@gmail.com>
2023-09-09 08:26:50 +0530

    Expense Scripts

--- frontend.sh
+++ frontend.sh
@@ -3,12 +3,16 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
+stat_check
+
 
 echo Placing Expense Config File in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &&>>$log_file
+stat_check
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &&>>$log_file
+stat_check
 
 cd /usr/share/nginx/html
 
@@ -17,4 +21,4 @@ download_and_extract
 echo Starting Nginx Service
 systemctl enable nginx &&>>$log_file
 systemctl restart nginx &&>>$log_file
-
+stat_check
--- mysql.sh
+++ mysql.sh
@@ -2,18 +2,22 @@ source common.sh
 
 echo Disable MySQL 8 Version
 dnf module disable mysql -y &>>$log_file
+stat_check
 
 echo Copy MySQL Repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+stat_check
 
 echo Install MySQL Server
 dnf install mysql-community-server -y &>>$log_file
+stat_check
 
 echo Start MySQL Service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
+stat_check
 
 echo Setup root Password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-
+stat_check
 
