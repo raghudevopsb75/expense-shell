75ee3d713f16ba2ccb8d86f2036db719f455f448
Raghu K <raghuk.vit@gmail.com>
2023-09-09 07:54:48 +0530

    Expense Scripts

--- backend.sh
+++ backend.sh
@@ -4,33 +4,33 @@ component=backend
 echo Install NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo FAILED
+  echo -e "\e[31mFAILED\e[0m"
 fi
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo FAILED
+  echo -e "\e[31mFAILED\e[0m"
 fi
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo FAILED
+  echo -e "\e[31mFAILED\e[0m"
 fi
 
 echo Add Application User
 useradd expense &>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo FAILED
+  echo -e "\e[31mFAILED\e[0m"
 fi
 
 echo Clean App Content
