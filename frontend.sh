cd327842f3ff5e93cff3395161694f6d94b03d76
Raghu K <raghuk.vit@gmail.com>
2023-09-09 08:34:35 +0530

    Expense Scripts

--- frontend.sh
+++ frontend.sh
@@ -7,11 +7,11 @@ stat_check
 
 
 echo Placing Expense Config File in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf &&>>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 stat_check
 
 echo Removing Old Nginx content
-rm -rf /usr/share/nginx/html/* &&>>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 stat_check
 
 cd /usr/share/nginx/html
@@ -19,6 +19,6 @@ cd /usr/share/nginx/html
 download_and_extract
 
 echo Starting Nginx Service
-systemctl enable nginx &&>>$log_file
-systemctl restart nginx &&>>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 stat_check
